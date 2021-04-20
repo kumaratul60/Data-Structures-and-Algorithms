c0264d24b5c08e8d59a978f9bb1cc7c8bb577688
Atul kumar <53579888+kumaratul60@users.noreply.github.com>
2021-04-20 14:07:56 +0530

    Update CatalnNumber.java

--- MagicNumber/CatalnNumber.java
+++ MagicNumber/CatalnNumber.java
@@ -1,3 +1,33 @@
+/*
+Catlan numbers are the sequence of natural numbers, which occurs in the form of various counting number problems. cn=1n+1(2nn)=2n! (n+1)! n!
+
+The Catalan numbers are a sequence of positive integers that appear in many counting problems in combinatorics. 
+They count certain types of lattice paths, permutations, binary trees, and many other combinatorial objects.
+
+
+Example
+
+->Taking and input n.
+->Check If n <= 1 then, Return 1.
+->Loop from i=0 to i<n and i++
+->For every i Set result = result + (catalan(i)*catalan(n-i-1))
+->Return and print the result.
+
+
+ Catalan numbers are a sequence of positive integers, where the nth term in the sequence, denoted Cn, is found in the following formula:
+
+Cn = (2n)! / ((n + 1)!n!)
+Here n!, pronounced n factorial, is equal to the product of all of the integers from n down to 1.
+
+(n) ⋅ (n - 1) ⋅ (n - 2) ⋅ … ⋅ 2 ⋅ 1
+
+
+Application: Pentagon Problem
+
+Catalan numbers are directly related to how many ways we can split an n-gon into triangles by connecting vertices where no two line segments cross.
+The number of possibilities is equal to Cn - 2.
+
+*/
 package MagicNumber;
 
 public class CatalnNumber {
