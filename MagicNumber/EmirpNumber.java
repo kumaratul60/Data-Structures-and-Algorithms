5a2c0551c19091ee19cc2aa97e71af982ef20f12
Atul kumar <53579888+kumaratul60@users.noreply.github.com>
2021-04-20 14:18:46 +0530

    Add files via upload

---
+++ MagicNumber/EmirpNumber.java
@@ -0,0 +1,49 @@
+package MagicNumber;
+
+import java.io.IOException;
+
+public class EmirpNumber {
+
+	public static boolean isPrime(int n) {
+		// Corner case
+		if (n <= 1)
+			return false;
+
+		// Check from 2 to n-1
+		for (int i = 2; i < n; i++)
+			if (n % i == 0)
+				return false;
+
+		return true;
+	}
+
+	// Function will check whether number
+	// is Emirp or not
+	public static boolean isEmirp(int n) {
+		// Check if n is prime
+		if (isPrime(n) == false)
+			return false;
+
+		// Find reverse of n
+		int rev = 0;
+		while (n != 0) {
+			int d = n % 10;
+			rev = rev * 10 + d;
+			n /= 10;
+		}
+
+		// If both Original and Reverse are Prime,
+		// then it is an Emirp number
+		return isPrime(rev);
+	}
+
+	// Driver Function
+	public static void main(String args[]) throws IOException {
+		int n = 13; // Input number
+		if (isEmirp(n) == true)
+			System.out.println("Yes");
+		else
+			System.out.println("No");
+	}
+
+}
